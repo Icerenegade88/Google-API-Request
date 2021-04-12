ff78ef131e8129bbcc998624cbcefaa95832d144
Alex <alexrapino75@gmail.com>
2021-04-11 21:50:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 #This will automatically install all modules required
-
-
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -27,8 +25,6 @@ def installlinux():
             print(color.RED+ "##################################################################" + color.END)
             exit(color.RED + f"This module failed: {module}" + color.END) 
     return()
-
-
 def installwindows():
     import os
     import subprocess
@@ -43,8 +39,6 @@ def installwindows():
             print(color.RED+ "##################################################################" + color.END)
             exit(color.RED + f"This module failed: {module}" + color.END) 
     return()
-
-
 def main():
     import platform
     import os
@@ -65,31 +59,22 @@ def main():
             installwindows()
         else:
             print("You have an outdated version of python please consider updating to 3.8 or later")
-        
-    
-    
     import urllib.parse
     import requests
     import pandas
     main_api = 'https://maps.googleapis.com/maps/api/geocode/json?'
-      
     key = input("Please Enter your API Key: ") #YOUR API KEY WILL BE LISTED IN THE Google Cloud: https://console.cloud.google.com/apis/credentials
-   
     address = input("Please enter the Address: ")
     url = main_api + urllib.parse.urlencode({'address': address, 'key' : key})
     print(url)
     json_data = requests.get(url).json()
-
     json_status = json_data['status']
     #Checking API
     if json_status == 'OK':
         formatted_address= json_data['results'][0]['formatted_address']
-
         #Grabbing lat lng coords from json file
         lat = json_data['results'][0]['geometry']['location']['lat']
         lng = json_data['results'][0]['geometry']['location']['lng']
-
         print(color.BOLD + formatted_address + color.END)
         print(color.RED + f"{lat}" + color.END, color.YELLOW + f"{lng}" + color.END)
-
 main()
