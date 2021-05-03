ff70869eb4da09df17201e63bbc883562cf8da6a
Icerenegade88 <alexrapino75@gmail.com>
2021-05-03 18:16:27 -0500

    Fun Stuff

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
-#This will automatically install all modules required
+# This will automatically install all modules required
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-## IN LINUX IT WILL ONLY USE PYTHON3 IF YOU HAVE A DEPRICATED VERSION OF PYTHON YOU NEED TO CHANGE THE COMMAND IN CODE
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+# IN LINUX IT WILL ONLY USE PYTHON3 IF YOU HAVE A DEPRICATED VERSION OF PYTHON YOU NEED TO CHANGE THE COMMAND IN CODE
+
 def installlinux():
     import os
     import subprocess
-    modules=['pandas', 'urllib3', 'requests', 'ipywidgets', 'IPython']
+    modules = ['pandas', 'urllib3', 'requests', 'ipywidgets', 'IPython']
     for module in modules:
         try:
-            subprocess.check_output(f"sudo python3 -m pip install {module}", stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output(
+                f"sudo python3 -m pip install {module}", stderr=subprocess.STDOUT, shell=True)
             print(f"{color.YELLOW + module + color.END} *OK*")
         except:
-            print(color.YELLOW + f"Please make sure you have the most recent version of python or these modules are correct {modules}" + color.END)
-            print(color.YELLOW + "If not you can download it here: https://www.python.org/downloads/ " + color.END)
-            print(color.RED+ "##################################################################" + color.END)
-            exit(color.RED + f"{module} *FAILED*" + color.END) 
+            print(
+                color.YELLOW + f"Please make sure you have the most recent version of python or these modules are correct {modules}" + color.END)
+            print(
+                color.YELLOW + "If not you can download it here: https://www.python.org/downloads/ " + color.END)
+            print(
+                color.RED + "##################################################################" + color.END)
+            exit(color.RED + f"{module} *FAILED*" + color.END)
     pd_setup()
+
 def installwindows():
     import os
     import subprocess
-    modules=['urllib3', 'requests', 'pandas', 'ipywidgets', 'IPython']
+    modules = ['urllib3', 'requests', 'pandas', 'ipywidgets', 'IPython']
     for module in modules:
         try:
-            subprocess.check_output(f"pip install {module}", stderr=subprocess.STDOUT, shell=True)
-            print(f"{color.YELLOW + module + color.END}" + color.GREEN + " *OK* " + color.END)
+            subprocess.check_output(
+                f"pip install {module}", stderr=subprocess.STDOUT, shell=True)
+            print(f"{color.YELLOW + module + color.END}" +
+                  color.GREEN + " *OK* " + color.END)
         except:
-            print(color.YELLOW + f"Please make sure you have the most recent version of python or these modules are correct {modules}" + color.END)
-            print(color.YELLOW + "If not you can download it here: https://www.python.org/downloads/ " + color.END)
-            print(color.RED+ "##################################################################" + color.END)
-            exit(color.RED + f"{module} *FAILED*" + color.END) 
+            print(
+                color.YELLOW + f"Please make sure you have the most recent version of python or these modules are correct {modules}" + color.END)
+            print(
+                color.YELLOW + "If not you can download it here: https://www.python.org/downloads/ " + color.END)
+            print(
+                color.RED + "##################################################################" + color.END)
+            exit(color.RED + f"{module} *FAILED*" + color.END)
     pd_setup()
+
 def version_check():
     import platform
     import os
@@ -52,7 +64,7 @@ def version_check():
     else:
         pythonver = subprocess.check_output("python -V", shell=True)
         pythonver = pythonver.decode("utf-8")
-        
+
     if system_report == "Linux" or system_report == "linux":
         os.system('sudo python3 -m pip install regex')
         import regex
@@ -65,20 +77,21 @@ def version_check():
         if regex.match(r'Python 3\.([8-9]|([1-9][0-9])?)\.([2-9]|([1-9][0-9])?)', pythonver):
             installwindows()
         else:
-            print("You have an outdated version of python please consider updating to 3.8 or later")
+            print(
+                "You have an outdated version of python please consider updating to 3.8 or later")
     return print(f"{color.YELLOW}Version: {color.END}{color.PURPLE}{system_report} {pythonver}" + color.GREEN + "OK" + color.END)
+
 def pd_setup():
     import pandas as pd
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
-    return print(color.YELLOW + "Pandas Setup: " +color.END + color.GREEN + "*OK*" + color.END)
+    return print(color.YELLOW + "Pandas Setup: " + color.END + color.GREEN + "*OK*" + color.END)
+
 def main():
-    import time
-    time.sleep(5)
     version_check()
-    
+
     import urllib3
     import urllib.parse
     import requests
@@ -86,22 +99,25 @@ def main():
     from IPython.display import display
     import build_url
     http = urllib3.PoolManager()
-        
+
     url = build_url.first_url.full_url()
     print(url)
     import pandas
     json_data = http.request('GET', f'{url}')
     df = json.loads(json_data.data.decode('utf-8'))
-    
+
     json_status = df['status']
-    #Checking API JSON Status
+    # Checking API JSON Status
     if json_status == 'OK':
         df = pandas.json_normalize(df['results'])
-        
-        lat = df['geometry.location.lat'] #I SPENT TWO HOURS TRYING TO FIGURE OUT HOW TO TURN A SERIES ITEM TO AN INT BECAUSE FOR SOME RESON IF YOU PRINT THIS
-        lng = df['geometry.location.lng'] #IT WILL COME BACK AS A SERIES AND STILL HOLD A FLOAT VALUE BUT MESS UP THE PRINT STATEMENT
 
-        #### THIS CONVERTS THE SERIES ITEMS INTO A FLOAT BECAUSE IT WAS BEING STUPID AND I DONT KNOW A BETTER WAY
+        # I SPENT TWO HOURS TRYING TO FIGURE OUT HOW TO TURN A SERIES ITEM TO AN INT BECAUSE FOR SOME RESON IF YOU PRINT THIS
+        lat = df['geometry.location.lat']
+        # IT WILL COME BACK AS A SERIES AND STILL HOLD A FLOAT VALUE BUT MESS UP THE PRINT STATEMENT
+        lng = df['geometry.location.lng']
+        
+        # THIS CONVERTS THE SERIES ITEMS INTO A FLOAT BECAUSE IT WAS BEING STUPID AND I DONT KNOW A BETTER WAY
+        #'''
         lat = lat.tolist()
         lng = lng.tolist()
         for la in lat:
@@ -109,9 +125,11 @@ def main():
         for ln in lng:
             lng = float(ln)
         ############################################################################################################
-        display(df[['formatted_address', 'geometry.location.lat', 'geometry.location.lng']])
-        lat = df['geometry.location.lat']
+        #'''
+        display(
+            df[['formatted_address', 'geometry.location.lat', 'geometry.location.lng']])
     ###### WHAT IS ABOVE IS MUCH BETTER THAN THE GARBO I HAD TO DEAL WITH BELOW ############
+    
     '''
     #Checking API
     if json_status == 'OK':
